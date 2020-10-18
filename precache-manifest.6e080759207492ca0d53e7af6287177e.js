0a84570092ecd66926f997124274bacc9cf3f763
Tien <nntien@apcs.vn>
2020-10-18 16:39:43 +0700

    [ci skip] Updates

---
+++ precache-manifest.6e080759207492ca0d53e7af6287177e.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d86dbce3d9031beed196daa2d5f93c1f",
+    "url": "/front-end-javascript-challenge/index.html"
+  },
+  {
+    "revision": "745017c7c1317e5ce9a9",
+    "url": "/front-end-javascript-challenge/static/css/2.a791b4d1.chunk.css"
+  },
+  {
+    "revision": "745017c7c1317e5ce9a9",
+    "url": "/front-end-javascript-challenge/static/js/2.bcae9003.chunk.js"
+  },
+  {
+    "revision": "e4c7c3a9928907cb06bcd3fd4b5152fc",
+    "url": "/front-end-javascript-challenge/static/js/2.bcae9003.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf01c63832e8f7ff07c5",
+    "url": "/front-end-javascript-challenge/static/js/main.7764d550.chunk.js"
+  },
+  {
+    "revision": "31cf4e630a3167ecfb20",
+    "url": "/front-end-javascript-challenge/static/js/runtime-main.348c424e.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/front-end-javascript-challenge/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/front-end-javascript-challenge/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/front-end-javascript-challenge/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/front-end-javascript-challenge/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/front-end-javascript-challenge/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/front-end-javascript-challenge/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/front-end-javascript-challenge/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/front-end-javascript-challenge/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/front-end-javascript-challenge/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/front-end-javascript-challenge/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/front-end-javascript-challenge/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/front-end-javascript-challenge/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/front-end-javascript-challenge/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/front-end-javascript-challenge/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/front-end-javascript-challenge/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/front-end-javascript-challenge/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
