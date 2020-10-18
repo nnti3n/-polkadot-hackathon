ef4956e0dbf99a92e00debf9b7a4a24d9fe11317
Tien <nntien@apcs.vn>
2020-10-18 14:38:12 +0700

    Better code

--- backend-javascript-challenge/cli.js
+++ backend-javascript-challenge/cli.js
@@ -1,22 +1,22 @@
-#!/usr/bin/env node
 const { ApiPromise, WsProvider } = require("@polkadot/api");
 
 async function main() {
   const params = process.argv.slice(2);
-  console.log(params);
 
   if (params.length > 2) {
-    throw Error("wrong number of arguments");
+    throw Error("Invalid arguments");
   }
-  const api = await ApiPromise.create();
+  const provider = new WsProvider('wss://rpc.polkadot.io');
+  const api = await ApiPromise.create({ provider });
+
   if (params.length == 1 && params[0] == 'get_latest_block') {
     const header = await api.rpc.chain.getHeader();
-    console.log(`Latest block number is ${header.number}`)
+    console.log(`Latest block number: ${header.number}`)
   } else if (params.length == 2 && params[0] == 'get_block') {
     const block = await api.rpc.chain.getBlock(params[1]);
     console.log(`Block ${params[1]}:`, JSON.stringify(block, null, 2));
   } else {
-    throw Error("invalid arguments")
+    throw Error("Invalid")
   }
 }
 main().catch(console.error);
